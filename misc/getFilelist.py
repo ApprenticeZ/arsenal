d7004b8a3be112662d92ac4a9d238938c4e78cbd
apprenticez <rookiecoder.z@gmail.com>
2013-07-08 10:55:24 +0800

    first commit

---
+++ misc/getFilelist.py
@@ -0,0 +1,24 @@
+# !/usr/bin/env python
+# encoding=utf-8
+
+'''
+Function: Go through a directory and save the file in the dir into a txt file
+Input: directory
+Output: an txt file stores all the filenames in the specified dir
+The first line specifies the directory
+The following lines record the file names
+
+Author: ApprenticeZ
+'''
+
+import os
+import os.path
+
+dataset_directory = r"I:\AVIP\workspace\dataset\an\pos"
+out = open(os.path.join(r"I:\AVIP\workspace\dataset\an", "poslist.txt"), 'w')
+for parent, dirnames, filenames in os.walk(dataset_directory):
+	out.write(parent+"\n");
+	for imname in filenames:
+		out.write(imname+"\n")
+		
+out.close()
---
+++ misc/paser.py
@@ -0,0 +1,49 @@
+# !/usr/bin/env python
+# encoding=utf-8
+
+'''
+This piece of code is used to transform the ground truth in xml format to txt format. 
+It is designed to parse CAVIAR Dataset, and can be quickly adapted to other dataset. 
+
+Input: filename of the xml file
+Output: a series of txt files, each contains ground truth of an image in the dataset
+Each row contains 6 data fields, the id of the target, height and weight of the bounding box, 
+the center of bbox in (xc, yc) format and its appearance (0: appear, 1:visible, 2:disapear, 3:occluded)
+
+Configs:
+- infilePath: specify the filename of the xml file
+- ofilePath: path to store the parsed txt files
+
+Author: ApprenticeZ
+'''
+
+from xml.etree import ElementTree
+
+infilePath = r"I:\AVIP\fore-background-detection\cwbs1gt.xml"
+ofilePath = r"I:\AVIP\fore-background-detection\WalkByShop1cor\frame"
+appearanceState = {'appear':0, 'visible':1, 'disappear':2, 'occluded':3}
+xmldoc = ElementTree.parse(infilePath)
+framelist = xmldoc.getiterator('frame')
+
+for fr in framelist:
+	mat = open(ofilePath+fr.get('number')+".txt", 'w')
+	# print 'frame ', fr.get('number')
+	olist = fr.getiterator('object')
+	for obj in olist:
+		id = obj.get('id')
+		mat.write(id)
+		mat.write('\t')
+		box = obj.find('box').attrib
+		mat.write(box['h'])
+		mat.write('\t')
+		mat.write(box['w'])
+		mat.write('\t')
+		mat.write(box['xc'])
+		mat.write('\t')
+		mat.write(box['yc'])
+		mat.write('\t')
+		appearance = obj.find('appearance').text
+		mat.write(str(appearanceState[appearance]))
+		mat.write('\n')
+	# glist = fr.getiterator('group')
+	mat.close()
